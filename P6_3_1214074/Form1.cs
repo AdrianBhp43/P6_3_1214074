ae848a28621d29d5293d4773a0be7e0d129c5204
Adorian43 <adrianbimo43@gmail.com>
2022-10-24 16:21:41 +0700

    Add project files.

---
+++ P6_3_1214074/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace P6_3_1214074
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtHuruf_Leave(object sender, EventArgs e)
+        {
+            if (txtHuruf.Text == "")
+            {
+                epWarning.SetError(txtHuruf, "Textbox Huruf tidak boleh kosong!");
+                epWrong.SetError(txtHuruf, "");
+                epCorrect.SetError(txtHuruf, "");
+            }
+            else
+            {
+                if ((txtHuruf.Text).All(Char.IsLetter))
+                {
+                    epWarning.SetError(txtHuruf, "");
+                    epWrong.SetError(txtHuruf, "");
+                    epCorrect.SetError(txtHuruf, "Betul!");
+                }
+                else
+                {
+                    epWrong.SetError(txtHuruf, "Inputan hanya boleh huruf!");
+                    epWarning.SetError(txtHuruf, "");
+                    epCorrect.SetError(txtHuruf, "");
+                }
+            }
+        }
+        private void txtAngka_Leave(object sender, EventArgs e)
+        {
+            if (txtAngka.Text == "")
+            {
+                epWarning.SetError(txtAngka, "Textbox Angka tidak boleh kosong!");
+                epWrong.SetError(txtAngka, "");
+                epCorrect.SetError(txtAngka, "");
+            }
+            else
+            {
+                if (txtAngka.Text == "")
+                {
+                    epWarning.SetError(txtAngka, "Textbox Angka tidak boleh kosong!");
+                    epWrong.SetError(txtAngka, "");
+                    epCorrect.SetError(txtAngka, "");
+                }
+                else
+                    if ((txtAngka.Text).All(Char.IsNumber))
+                {
+                    epCorrect.SetError(txtAngka, "Betul!");
+                    epWarning.SetError(txtAngka, "");
+                    epWrong.SetError(txtAngka, "");
+                }
+                else
+                {
+                    epCorrect.SetError(txtAngka, "");
+                    epWarning.SetError(txtAngka, "");
+                    epWrong.SetError(txtAngka, "Inputan hanya boleh Angka!");
+                }
+            }
+        }
+        private void txtEmail_Leave(object sender, EventArgs e)
+        {
+
+            if (txtEmail.Text == "")
+            {
+                epWarning.SetError(txtEmail, "Textbox Email tidak boleh kosong!");
+                epWrong.SetError(txtEmail, "");
+                epCorrect.SetError(txtEmail, "");
+            }
+            else
+            {
+                if (Regex.IsMatch(txtEmail.Text, @"^^[^@\s]+@[^@\s]+(\.[^@\s]+)+$"))
+                {
+                    epWarning.SetError(txtEmail, "");
+                    epWrong.SetError(txtEmail, "");
+                    epCorrect.SetError(txtEmail, "Betul!");
+                }
+                else
+                {
+                    epWrong.SetError(txtEmail, "Format email salah!\nContoh: a@b.c ");
+                    epWarning.SetError(txtEmail, "");
+                    epCorrect.SetError(txtEmail, "");
+                }
+            }
+        }
+
+        private void txtAngka1_Leave(object sender, EventArgs e)
+        {
+            if (txtAngka1.Text == "")
+            {
+                epWarning.SetError(txtAngka1, "Textbox Angka tidak boleh kosong!");
+                epWrong.SetError(txtAngka1, "");
+                epCorrect.SetError(txtAngka1, "");
+            }
+            else
+            {
+                if ((txtAngka1.Text).All(Char.IsNumber))
+                {
+                    epWarning.SetError(txtAngka1, "");
+                    epWrong.SetError(txtAngka1, "");
+                    epCorrect.SetError(txtAngka1, "Betul!");
+
+                }
+                else
+                {
+                    epCorrect.SetError(txtAngka1, "");
+                    epWrong.SetError(txtAngka1, "Inputan hanya boleh Angka!");
+                    epWarning.SetError(txtAngka1, "");
+                }
+            }
+                if (txtAngka2.Text == "")
+                {
+                    epWarning.SetError(txtAngka1, "Inputan hanya boleh Angka!");
+                    epCorrect.SetError(txtAngka1, "");
+                    epWrong.SetError(txtAngka1, "");
+                }
+            }
+        
+
+        private void txtAngka2_Leave(object sender, EventArgs e)
+        {
+            int Angka1 = int.Parse(txtAngka1.Text);
+            int Angka2 = int.Parse(txtAngka2.Text);
+
+
+            if (txtAngka2.Text == "")
+            {
+                epWarning.SetError(txtAngka2, "Textbox Angka tidak boleh kosong!");
+                epWrong.SetError(txtAngka2, "");
+                epCorrect.SetError(txtAngka2, "");
+            }
+            else
+            {
+                if ((txtAngka2.Text).All(Char.IsNumber))
+                {
+                    epCorrect.SetError(txtAngka2, "Betul!");
+                    epWarning.SetError(txtAngka2, "");
+                    epWrong.SetError(txtAngka2, "");
+                }
+                else
+                {
+                    epCorrect.SetError(txtAngka2, "");
+                    epWarning.SetError(txtAngka2, "");
+                    epWrong.SetError(txtAngka2, "Inputan hanya boleh Angka!");
+                }
+            } 
+        
+                if (Angka1 > Angka2)
+                {
+                    epWarning.SetError(txtAngka2, "Betul!");
+                    epCorrect.SetError(txtAngka2, "");
+                    epWrong.SetError(txtAngka2, "");
+
+                    epWarning.SetError(txtAngka2, "");
+                    epWrong.SetError(txtAngka2, "");
+                    epCorrect.SetError(txtAngka2, "Betul!");
+                }
+                else
+                {
+                    epWarning.SetError(txtAngka2, "Salah!");
+                    epCorrect.SetError(txtAngka2, "");
+                    epWrong.SetError(txtAngka2, "");
+
+                    epWarning.SetError(txtAngka2, "");
+                    epWrong.SetError(txtAngka2, "");
+                    epCorrect.SetError(txtAngka2, "Salah!");
+                }
+            }
+        }
+    }
